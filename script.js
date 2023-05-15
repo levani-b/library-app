ecc44d3954173b6a7f3a230886fe56a47b277ed0
Levani Birchadze <levanbirchadze@gmail.com>
2023-05-15 14:36:43 +0400

    add local storage

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 let myLibrary = [];
 
 class Book {
-  constructor(title, author){
+  constructor(title, author,pages){
     this.title = title;
     this.author = author;
+    this.pages = pages;
   }
 }
 
-const addBookToLibrary = (title,author) =>{
-  const newBook = new Book(title,author);
+const addBookToLibrary = (title,author,pages) =>{
+  const newBook = new Book(title,author,pages);
    myLibrary.push(newBook);
+   saveLibraryToLocalStorage();
 }
 
 const bookTitle = document.getElementById('title');
 const bookAuthor = document.getElementById('author');
+const bookPages = document.getElementById('pages');
 const submitBtn = document.getElementById('submit-btn');
 const bookContainer = document.getElementById('books-container');
 
@@ -22,22 +25,52 @@ const getInfoFromInputs = (event) => {
 
     let titleValue = bookTitle.value;
     let authorValue = bookAuthor.value;
+    let pagesValue = parseInt(bookPages.value);
 
-    addBookToLibrary(titleValue,authorValue);
-    makeElements(titleValue, authorValue);
+    addBookToLibrary(titleValue,authorValue,pagesValue);
+    renderBooks();
+    clearInputFields();
 }
 
-const makeElements = (title, author) => {
-    const titleOutput = document.createElement('p');
-    const authorOutput = document.createElement('p');
+const renderBooks = () => {
+    bookContainer.innerHTML = '';
 
-    titleOutput.textContent = title;
-    authorOutput.textContent = author;
+    myLibrary.forEach(book => {
+        const bookElement = document.createElement('div');
+        bookElement.classList.add('book');
 
+        const titleOutput = document.createElement('p');
+        titleOutput.textContent = `Title : ${book.title}`;
 
-    bookContainer.appendChild(titleOutput);
-    bookContainer.appendChild(authorOutput);
+        const authorOutput = document.createElement('p');
+        authorOutput.textContent = `Author : ${book.author}`
+
+        const pagesOutput = document.createElement('p')
+        pagesOutput.textContent = `Pages : ${book.pages}`
+
+        bookElement.appendChild(titleOutput);
+        bookElement.appendChild(authorOutput);
+        bookElement.appendChild(pagesOutput);
+
+        bookContainer.appendChild(bookElement);
+    })
+}
+const clearInputFields = () => {
+    bookTitle.value = '';
+    bookAuthor.value = '';
+    bookPagesInput.value = '';
 }
 
-submitBtn.addEventListener('click', getInfoFromInputs);
+const saveLibraryToLocalStorage = () => {
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+}
+const loadLibraryFromLocalStorage = () => {
+    const libraryData = localStorage.getItem('myLibrary');
+    if (libraryData) {
+      myLibrary = JSON.parse(libraryData);
+      renderBooks();
+    }
+}
+submitBtn.addEventListener('submit', getInfoFromInputs);
+window.addEventListener('load', loadLibraryFromLocalStorage);
 console.log(myLibrary);
