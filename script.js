335705ba96879c2d781f9b2f5dfeb318cde5de23
levani-b <levanbirchadze@gmail.com>
2025-05-09 17:38:02 +0400

    render books to the page using DOM manipulation

--- script.js
+++ script.js
@@ -15,11 +15,38 @@ function addBookToLibrary(title, author, pages, isRead) {
   myLibrary.push(newBook);
 }
 
+function renderBooksToPage(library) {
+  const libraryContainer = document.querySelector(".library");
+  libraryContainer.innerHTML = "";
+
+  library.forEach((book) => {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+
+    const title = document.createElement("p");
+    title.textContent = `Title: ${book.title}`;
+
+    const author = document.createElement("p");
+    author.textContent = `Author: ${book.author}`;
+
+    const pages = document.createElement("p");
+    pages.textContent = `Pages: ${book.pages}`;
+
+    const isRead = document.createElement("div");
+    isRead.textContent = `Read: ${book.isRead ? "Yes" : "No"}`;
+
+    bookCard.appendChild(title);
+    bookCard.appendChild(author);
+    bookCard.appendChild(pages);
+    bookCard.appendChild(isRead);
+
+    libraryContainer.appendChild(bookCard);
+  });
+}
+
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, true);
 addBookToLibrary("1984", "George Orwell", 328, false);
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, true);
 addBookToLibrary("1984", "George Orwell", 328, false);
 
-myLibrary.forEach((element) => {
-  console.log(element.id);
-});
+renderBooksToPage(myLibrary);
